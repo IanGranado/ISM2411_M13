d504bee730b34deadbecd495c9511300cde4b2c4
Ian R Granado <igranado@usf.edu>
2025-11-23 21:38:45 -0500

    Clean up code, add comments, finalize program

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+## test print
 print("Hello World, Welcome to my Python program!")
 
+## requesting the hours inputted
 todays_hours = input("How many hours were you working for today? ")
 
+## converting hours into a numerical float value.
+## verifies that a number, and only a number, is given.
 try:
     todays_hours = float(todays_hours)
 except ValueError:
     print("Uh-Oh, only numeric values are accepted! Try again using numbers only.")
     exit()
 
-weekly_total = todays_hours * 7
+##Multiplication
+weekly_total = (todays_hours * 7) ##fixed multiplier
 
-print(f"Based off your input, if you work the same hours daily (for 7 days), you are on track to work {todays_hours:.2f} hours this week.")
+## Final result. Rounded down to two decimals as my payroll does.
+print(f"Based off your input, if you work the same hours daily (for 7 days), you are on track to work {weekly_total:.2f} hours this week.")
