e90d1d864b16fe817dc85058cb895d01f9849ab8
Ian R Granado <igranado@usf.edu>
2025-11-23 21:33:08 -0500

    Add formatted output displaying calculation results

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ todays_hours = float(todays_hours)
 
 weekly_total = todays_hours * 7
 
-print(f"Based off your input, if you work the same hours daily (for 7 days), you are on track to work {todays_hours} hours this week.")
+print(f"Based off your input, if you work the same hours daily (for 7 days), you are on track to work {todays_hours:.2f} hours this week.")
