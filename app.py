187c8bb67117a7105c0e62e1ee60f61eb8c6e6cc
Ian R Granado <igranado@usf.edu>
2025-11-23 21:28:16 -0500

    Add formatted output displaying calculation results

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ todays_hours = input("How many hours were you working for today? ")
 todays_hours = float(todays_hours)
 
 weekly_total = todays_hours * 7
+
+print(f"Based off your input, if you work the same hours daily (for 7 days), you are on track to work {todays_hours} hours this week.")
