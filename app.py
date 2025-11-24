f18f792413613fa39150989920fbecf88d68bdcd
Ian R Granado <igranado@usf.edu>
2025-11-23 21:26:58 -0500

    Add calculation based on user input

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 print("Hello World, Welcome to my Python program!")
 
 todays_hours = input("How many hours were you working for today? ")
+
+todays_hours = float(todays_hours)
+
+weekly_total = todays_hours * 7
